628c4509cfbc2c40b4a76bbce659b8b87a645e34
Lenny0939 <lennyescott@gmail.com>
2025-06-14 21:03:01 +1000

    niri stuffgit add .

--- home/niri.nix
+++ home/niri.nix
@@ -1,25 +1,33 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let
   startup =
     with pkgs;
     writeShellScriptBin "startup" ''
-      ${pkgs.uwsm}/bin/uwsm app -- ${hyprpaper}/bin/hyprpaper
-      ${pkgs.uwsm}/bin/uwsm app -- ${eww}/bin/eww open bar
+      ${pkgs.uwsm}/bin/uwsm app -- ${swaybg}/bin/swaybg -m fill -i ${config.stylix.image} &
+      ${pkgs.uwsm}/bin/uwsm app -- ${eww}/bin/eww open bar &
+			${pkgs.xwayland-satellite}/bin/xwayland-satellite
     '';
 in
 {
   programs.niri = {
     settings = {
       spawn-at-startup = with pkgs; [
-        #"${hyprpaper}/bin/hyprpaper"
-        {
-          command = [
-            "${eww}/bin/eww"
-            "open"
-            "bar"
-          ];
-        }
-      ];
+				{
+					command = [ "${startup}/bin/startup" ];
+				}
+      ];    
+			environment = {
+				DISPLAY = ":0";
+			};
+			binds = with pkgs; with config.lib.niri.actions; {
+				"XF86AudioRaiseVolume".action = spawn "${pulseaudio}/bin/pactl" "set-sink-volume" "@DEFAULT_SINK@" "+10%";
+				"XF86AudioLowerVolume".action = spawn "${pulseaudio}/bin/pactl" "set-sink-volume" "@DEFAULT_SINK@" "-10%";
+				"Mod+S".action = spawn "wofi";
+				"Mod+F".action = spawn "ghostty";
+				#"Mod+S".action = spawn "${uwsm}/bin/uwsm" "app" "--" "$(${wofi}/bin/wofi" "--define-drun-print_desktop_file=true)";
+				#"Mod+F".action = spawn "${uwsm}/bin/uwsm" "app" "--" "${ghostty}";
+				"Mod+Q".action = quit;
+			};
     };
   };
 }
