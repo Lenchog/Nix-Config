58d17ef2543215f9bd92d356fbcec7b7c2803ec9
Lenny0939 <lennyescott@gmail.com>
2025-07-10 16:15:56 +1000

    removed archive

--- archive/slippi.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-  slippi-launcher = {
-    isoPath = "/users/lenny/home/Projects/Retro Games/melee/animelee.iso";
-    launchMeleeOnPlay = false;
-    enableJukebox = true;
-  };
-}
--- archive/xdg.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  xdg = {
-    configHome = "/users/lenny/config";
-    cacheHome = "/users/lenny/config/cache";
-    dataHome = "/users/lenny/config/local/share";
-    stateHome = "/users/lenny/config/local/state";
-  };
-}
