25a1aef7906df27b7d3603fe291e255870cd0523
Lenny0939 <lennyescott@gmail.com>
2025-07-10 16:10:04 +1000

    added way more epic backups

--- frodo/restic.nix
+++ frodo/restic.nix
@@ -1,23 +1,59 @@
-{ config, ... }:
+{ config, pkgs, ... }:
+let full = [
+	"/var/lib/garf/scores.db"
+	"/var/lib/syncthing"
+	"/var/lib/photoprism"
+	"/var/lib/vaultwarden"
+];
+minimal = [
+	"/var/lib/garf/scores.db"
+	"/var/lib/syncthing"
+	"/var/lib/vaultwarden"
+];
+pruneOpts = [
+	"--keep-daily 7"
+	"--keep-weekly 5"
+	"--keep-monthly 3"
+	"--keep-yearly 9999"
+];
+in
 {
   services.restic.backups = {
+	# idk why this is called this but don't wanna change it
     b2 = {
-      paths = [
-        "/var/lib/minecraft/final-clan/world"
-        "/var/lib/garf/scores.db"
-				"/var/lib/syncthing"
-				"/var/lib/photoprism"
-				"/var/lib/vaultwarden"
-      ];
+      paths = full;
       repository = "/var/restic";
       passwordFile = config.sops.secrets."resticPassword".path;
       initialize = true;
-      pruneOpts = [
-        "--keep-daily 7"
-        "--keep-weekly 5"
-        "--keep-monthly 12"
-        "--keep-yearly 75"
-      ];
-    };
-  };
+      pruneOpts = pruneOpts;
+  	};
+		hard-drive = {
+      paths = full;
+			repository = "/media/hard-drive/restic";
+      pruneOpts = pruneOpts;
+      passwordFile = config.sops.secrets."resticPassword".path;
+      initialize = true;
+  	};
+		google-drive = {
+      paths = minimal;
+			repository = "/media/google-drive/restic";
+      pruneOpts = pruneOpts;
+      passwordFile = config.sops.secrets."resticPassword".path;
+      initialize = true;
+  	};
+	};
+	systemd = {
+		timers.rclone = {
+			enable = true;
+			timerConfig = {
+				OnCalendar = "hourly";
+			};
+		};
+		services.rclone = {
+			description = "Backup restic backups to Google Drive";
+			serviceConfig = { 
+				ExecStart = "${pkgs.rclone}/bin/rclone copy /media/google-drive/restic google-drive:backup --config /root/.config/rclone/rclone.conf";
+			};
+		};
+	};
 }
