67d2f9e7b4f77abf72f2069ec4d8bf51db3445c4
Lenny0939 <lennyescott@gmail.com>
2025-07-12 16:46:55 +1000

    configured some niri stuff

--- home/niri.nix
+++ home/niri.nix
@@ -3,15 +3,16 @@ let
   startup =
     with pkgs;
     writeShellScriptBin "startup" ''
-      ${pkgs.uwsm}/bin/uwsm app -- ${swaybg}/bin/swaybg -m fill -i ${config.stylix.image} &
-      ${pkgs.uwsm}/bin/uwsm app -- ${eww}/bin/eww open bar &
-			${pkgs.xwayland-satellite}/bin/xwayland-satellite
+      ${uwsm}/bin/uwsm app -- ${swaybg}/bin/swaybg -m fill -i ${config.stylix.image} &
+      ${uwsm}/bin/uwsm app -- ${eww}/bin/eww open bar &
+			${xwayland-satellite}/bin/xwayland-satellite
     '';
 in
 {
   programs.niri = {
     settings = {
-      spawn-at-startup = with pkgs; [
+			prefer-no-csd = true;
+      spawn-at-startup = [
 				{
 					command = [ "${startup}/bin/startup" ];
 				}
@@ -19,14 +20,35 @@ in
 			environment = {
 				DISPLAY = ":0";
 			};
-			binds = with pkgs; with config.lib.niri.actions; {
-				"XF86AudioRaiseVolume".action = spawn "${pulseaudio}/bin/pactl" "set-sink-volume" "@DEFAULT_SINK@" "+10%";
-				"XF86AudioLowerVolume".action = spawn "${pulseaudio}/bin/pactl" "set-sink-volume" "@DEFAULT_SINK@" "-10%";
-				"Mod+S".action = spawn "wofi";
-				"Mod+F".action = spawn "ghostty";
-				#"Mod+S".action = spawn "${uwsm}/bin/uwsm" "app" "--" "$(${wofi}/bin/wofi" "--define-drun-print_desktop_file=true)";
-				#"Mod+F".action = spawn "${uwsm}/bin/uwsm" "app" "--" "${ghostty}";
-				"Mod+Q".action = quit;
+			animations = {
+				slowdown = 0.5;
+			};
+			layout = {
+				gaps = 0;
+				focus-ring = {
+					enable = false;
+				};
+			};
+			binds = with pkgs; with config.lib.niri.actions; let sh = spawn "sh" "-c"; in {
+				"XF86AudioRaiseVolume".action = sh "${wireplumber}/bin/wpctl set-volume @DEFAULT_SINK@ 0.1+";
+				"XF86AudioMute".action = sh "${wireplumber}/bin/wpctl set-mute @DEFAULT_SINK@ toggle";
+				"XF86AudioMicMute".action = sh "${wireplumber}/bin/wpctl set-mute @DEFAULT_SOURCE@ toggle";
+				"XF86AudioLowerVolume".action = sh "${wireplumber}/bin/wpctl set-volume @DEFAULT_SINK@ 0.1-";
+    		"XF86MonBrightnessUp".action = sh "${brightnessctl}/bin/brightnessctl set +10%";
+    		"XF86MonBrightnessDown".action = sh "${brightnessctl}/bin/brightnessctl set 10%-";
+				"Mod+S".action = sh "${uwsm}/bin/uwsm app -- $(${wofi}/bin/wofi --define-drun-print_desktop_file=true)";
+				"Mod+F".action = sh "${uwsm}/bin/uwsm app -- ${foot}/bin/footclient";
+				"Mod+Q".action = close-window;
+				"Mod+A".action = fullscreen-window;
+				"Mod+Shift+Q".action = quit;
+    		"Mod+H".action = focus-column-left; 
+    		"Mod+J".action = focus-window-down; 
+    		"Mod+K".action = focus-window-up; 
+    		"Mod+L".action = focus-column-right; 
+    		"Mod+Shift+H".action = move-column-left; 
+    		"Mod+Shift+J".action = move-window-down; 
+    		"Mod+Shift+K".action = move-window-up; 
+    		"Mod+Shift+L".action = move-column-right; 
 			};
     };
   };
