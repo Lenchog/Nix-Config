248776ce0ccd943d04a64107e4e256ffa0eaea33
Lenchog <lennyescott@gmail.com>
2026-02-28 09:33:34 +1100

    added more config for pc

--- modules/hosts/aragorn.nix
+++ modules/hosts/aragorn.nix
@@ -5,24 +5,56 @@
   ...
 }:
 {
-  flake.nixosConfigurations.aragorn = inputs.nixpkgs.lib.nixosSystem {
-    modules = [
-      self.modules.nixos.aragorn
-      self.modules.nixos.main
-      self.modules.nixos.gui
-    ];
-  };
-  flake.modules.nixos.aragorn = {
-    boot = {
-      initrd.kernelModules = [ "nvidia" ];
-      kernelParams = [ "nvidia-drm.modeset=1" ];
+  flake = {
+
+    nixosConfigurations.aragorn = inputs.nixpkgs.lib.nixosSystem {
+      modules = with self.modules.nixos; [
+        base
+        gui
+        boot
+        nix-conf
+        users
+        homeManager
+        networking
+        syncthing
+        aragorn
+        kanata
+        sops
+        nh
+      ];
     };
-    hardware.nvidia = {
-      open = false;
-      modesetting.enable = true;
-      # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+    modules.nixos.aragorn = {
+      boot = {
+        initrd = {
+          kernelModules = [ "nvidia" ];
+          availableKernelModules = [
+            "xhci_pci"
+            "ahci"
+            "nvme"
+            "usbhid"
+          ];
+        };
+        kernelParams = [
+          "nvidia-drm.modeset=1"
+          "kvm-amd"
+        ];
+      };
+      hardware.nvidia = {
+        open = false;
+        modesetting.enable = true;
+        # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+      };
+      services.xserver.videoDrivers = [ "nvidia" ];
+      networking.hostName = "aragorn";
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/ce6b7dd2-5df7-49b7-bd87-45c6c86ee0fb";
+        fsType = "ext4";
+      };
+
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/4239-6CA4";
+        fsType = "vfat";
+      };
     };
-    services.xserver.videoDrivers = [ "nvidia" ];
-    networking.hostName = "aragorn";
   };
 }
