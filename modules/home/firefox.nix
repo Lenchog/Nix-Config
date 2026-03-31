7647e2cb225fd739c81d906afb628ef1fc8fb651
Lenchog <lennyescott@gmail.com>
2026-03-31 18:28:31 +1100

    updated user.js

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -69,7 +69,7 @@
               **************************************************************************
                * Betterfox                                                                *
                * "Ad meliora"                                                             *
-               * version: 146                                                             *
+               * version: 149                                                             *
                * url: https://github.com/yokoffing/Betterfox                              *
               ***************************************************************************
             */
@@ -79,67 +79,7 @@
                * SECTION: FASTFOX                                                         *
               ***************************************************************************
             */
-            /**
-              GENERAL **
-            */
-            "gfx.content.skia-font-cache-size" = 32;
-
-            /**
-              GFX **
-            */
-            "gfx.webrender.layer-compositor" = true;
-            "gfx.canvas.accelerated.cache-items" = 32768;
-            "gfx.canvas.accelerated.cache-size" = 4096;
-            "webgl.max-size" = 16384;
-
-            /**
-              DISK CACHE **
-            */
-            "browser.cache.disk.enable" = false;
-
-            /**
-              MEMORY CACHE **
-            */
-            "browser.cache.memory.capacity" = 131072;
-            "browser.cache.memory.max_entry_size" = 20480;
-            "browser.sessionhistory.max_total_viewers" = 4;
-            "browser.sessionstore.max_tabs_undo" = 10;
-
-            /**
-              MEDIA CACHE **
-            */
-            "media.memory_cache_max_size" = 262144;
-            "media.memory_caches_combined_limit_kb" = 1048576;
-            "media.cache_readahead_limit" = 600;
-            "media.cache_resume_threshold" = 300;
-
-            /**
-              IMAGE CACHE **
-            */
-            "image.cache.size" = 10485760;
-            "image.mem.decode_bytes_at_a_time" = 65536;
-
-            /**
-              NETWORK **
-            */
-            "network.http.max-connections" = 1800;
-            "network.http.max-persistent-connections-per-server" = 10;
-            "network.http.max-urgent-start-excessive-connections-per-host" = 5;
-            "network.http.request.max-start-delay" = 5;
-            "network.http.pacing.requests.enabled" = false;
-            "network.dnsCacheEntries" = 10000;
-            "network.dnsCacheExpiration" = 3600;
-            "network.ssl_tokens_cache_capacity" = 10240;
-
-            /**
-              SPECULATIVE LOADING **
-            */
-            "network.http.speculative-parallel-limit" = 0;
-            "network.dns.disablePrefetch" = true;
-            "network.dns.disablePrefetchFromHTTPS" = true;
-            "browser.urlbar.speculativeConnect.enabled" = false;
-            "browser.places.speculativeConnect.enabled" = false;
-            "network.prefetch-next" = false;
+            "gfx.canvas.accelerated.cache-size" = 256;
 
             /**
               **************************************************************************
@@ -171,7 +111,9 @@
             /**
               DISK AVOIDANCE **
             */
+            "browser.cache.disk.enable" = false;
             "browser.privatebrowsing.forceMediaMemoryCache" = true;
+            "media.memory_cache_max_size" = 65536;
             "browser.sessionstore.interval" = 60000;
 
             /**
@@ -179,6 +121,16 @@
             */
             "privacy.history.custom" = true;
             "browser.privatebrowsing.resetPBM.enabled" = true;
+
+            /**
+              SPECULATIVE LOADING **
+            */
+            "network.http.speculative-parallel-limit" = 0;
+            "network.dns.disablePrefetch" = true;
+            "network.dns.disablePrefetchFromHTTPS" = true;
+            "browser.urlbar.speculativeConnect.enabled" = false;
+            "browser.places.speculativeConnect.enabled" = false;
+            "network.prefetch-next" = false;
 
             /**
               SEARCH / URL BAR **
@@ -304,6 +256,7 @@
             /**
               AI **
             */
+            "browser.ai.control.default" = "blocked";
             "browser.ml.enable" = false;
             "browser.ml.chat.enabled" = false;
             "browser.ml.chat.menu" = false;
@@ -351,52 +304,27 @@
 
             /**
               **************************************************************************
-               * START: MY OVERRIDES                                                      *
-              ***************************************************************************
-            */
-            /*
-              visit https://github.com/yokoffing/Betterfox/wiki/Common-Overrides
-              // visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
-              // Enter your personal overrides below this line:
-            */
-
-            /**
-              **************************************************************************
                * SECTION: SMOOTHFOX                                                       *
               ***************************************************************************
             */
-            /*
-              visit https://github.com/yokoffing/Betterfox/blob/main/Smoothfox.js
-              // Enter your scrolling overrides below this line:
-            */
+            # visit https://github.com/yokoffing/Betterfox/blob/main/Smoothfox.js
+            # Enter your scrolling overrides below this line:
+
             /**
-              **************************************************************************************
-               * OPTION: NATURAL SMOOTH SCROLLING V3 [MODIFIED]                                      *
-              ***************************************************************************************
+              **************************************************************************
+               * START: MY OVERRIDES                                                      *
+              ***************************************************************************
             */
-            /*
-              credit: https://github.com/AveYo/fox/blob/cf56d1194f4e5958169f9cf335cd175daa48d349/Natural%20Smooth%20Scrolling%20for%20user.js
-              // recommended for 120hz+ displays
-              // largely matches Chrome flags: Windows Scrolling Personality and Smooth Scrolling
-            */
-            "apz.overscroll.enabled" = true; # DEFAULT NON-LINUX
-            "general.smoothScroll" = true; # DEFAULT
-            "general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS" = 12;
-            "general.smoothScroll.msdPhysics.enabled" = true;
-            "general.smoothScroll.msdPhysics.motionBeginSpringConstant" = 600;
-            "general.smoothScroll.msdPhysics.regularSpringConstant" = 650;
-            "general.smoothScroll.msdPhysics.slowdownMinDeltaMS" = 25;
-            "general.smoothScroll.msdPhysics.slowdownMinDeltaRatio" = "2";
-            "general.smoothScroll.msdPhysics.slowdownSpringConstant" = 250;
-            "general.smoothScroll.currentVelocityWeighting" = "1";
-            "general.smoothScroll.stopDecelerationWeighting" = "1";
-            "mousewheel.default.delta_multiplier_y" = 300; # 250-400; adjust this number to your liking
+            # visit https://github.com/yokoffing/Betterfox/wiki/Common-Overrides
+            # visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
+            # Enter your personal overrides below this line:
 
             /**
               **************************************************************************
                * END: BETTERFOX                                                           *
               ***************************************************************************
             */
+
           };
           userChrome =
             # css
