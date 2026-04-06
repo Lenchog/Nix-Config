63a1d2df6de314d6c273fe35a11b1832ee6f8c11
Lenchog <lennyescott@gmail.com>
2026-04-06 10:55:27 +1000

    remove kitty

--- modules/home/lf.nix
+++ modules/home/lf.nix
@@ -78,31 +78,6 @@
           "dr" = "restore-trash";
           f = "find-dir";
         };
-        extraConfig =
-          let
-            previewer = pkgs.writeShellScriptBin "preview.sh" ''
-                file=$1
-                w=$2
-                h=$3
-                x=$4
-                y=$5
-
-                if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$file")" =~ ^image ]]; then
-                    ${pkgs.kitty}/bin/kitty +kitten icat --transfer-mode file --stdin no --place "''${w}x''${h}@''${x}x''${y}" "''$file" < /dev/null > /dev/tty
-              #kitten icat --silent --stdin no --transfer-mode file --place "''${w}x''${h}@''${x}x''${y}" "$file" < /dev/null > /dev/tty
-                    exit 1
-                fi
-
-                ${pkgs.pistol}/bin/pistol "$file"
-            '';
-            cleaner = pkgs.writeShellScriptBin "clean.sh" ''
-              ${pkgs.kitty}/bin/kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
-            '';
-          in
-          ''
-            set cleaner ${cleaner}/bin/clean.sh
-            set previewer ${previewer}/bin/preview.sh
-          '';
       };
       xdg.configFile."lf/icons".text = ''
         # vim:ft=conf
