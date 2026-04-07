18ef0a1d340ee00a90078b16241fb44eea9818b5
Lenchog <lennyescott@gmail.com>
2026-04-08 08:10:15 +1000

    add nix community cachix to trusted public keys

--- modules/nixosModules/nix.nix
+++ modules/nixosModules/nix.nix
@@ -8,7 +8,10 @@
             "nix-command"
             "flakes"
           ];
-          trusted-public-keys = [ "nix-key:hSWnFY8bjaxrQs/nUjirSXeNruuy1KW6fCY3cURGzfY=" ];
+          trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "nix-key:hSWnFY8bjaxrQs/nUjirSXeNruuy1KW6fCY3cURGzfY="
+          ];
           secret-key-files = [ config.sops.secrets."nix-key".path ];
           auto-optimise-store = true;
           trusted-users = [ "@wheel" ];
