b2576e479908e997d13f4cf09ac6cd3b9ab477ea
Lenchog <lennyescott@gmail.com>
2026-08-15 11:41:16 +1000

    maximise instead of fullscreen niri

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -55,7 +55,8 @@
               "Mod+F".action = sh "${uwsm}/bin/uwsm app -- ${foot}/bin/footclient";
               "Mod+W".action.screenshot = { };
               "Mod+Q".action = close-window;
-              "Mod+A".action = fullscreen-window;
+              "Mod+R".action = fullscreen-window;
+              "Mod+A".action = maximise-window-to-edges;
               "Mod+E".action = toggle-window-floating;
               "Mod+Shift+Q".action = quit;
               "Mod+H".action = focus-column-left;
